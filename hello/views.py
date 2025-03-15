572b68fd83a3dfdad6f89e4090c5c7de344d1af4
spanersoraferty <s_ohegarty@yahoo.co.uk>
2025-03-15 16:05:00 +0000

    first commit

---
+++ hello/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from datetime import datetime
+
+# Create your views here.
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
