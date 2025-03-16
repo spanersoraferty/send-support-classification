dea002580f952d6d38ca84b61c8455db1d331ecc
spanersoraferty <s_ohegarty@yahoo.co.uk>
2025-03-16 13:34:14 +0000

    another commit

--- web_project/settings.py
+++ web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j#&6_#@gb35)uvhrx#in81!5@o@m(9vs7ffo#r%)6jm1kt$gk%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['spanersoraferty.pythonanywhere.com']
 
 
 # Application definition
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
-
 ]
 
 MIDDLEWARE = [
@@ -118,8 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
